8ac90be0fe9dc582edb121fc04c7ccab25dd57ca
Ruslan Fedotov <progruson@gmail.com>
2025-08-19 21:35:12 +0700

    Add Malay lang

---
+++ lib/num2words/locales/ms.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Num2words
+  module Locales
+    module MS
+      ONES_MASC = I18n.t("num2words.ones_masc", locale: :ms)
+      ONES_FEM = I18n.t("num2words.ones_fem", locale: :ms)
+      TEENS = I18n.t("num2words.teens", locale: :ms)
+      TENS = I18n.t("num2words.tens", locale: :ms)
+      HUNDREDS = I18n.t("num2words.hundreds", locale: :ms)
+      SCALES = I18n.t("num2words.scales", locale: :ms)
+
+      RUB = I18n.t("num2words.currencies.MYR.name", locale: :ms)
+      KOP = I18n.t("num2words.currencies.MYR.fractional.name", locale: :ms)
+    end
+
+    register :ms, MS
+  end
+end
