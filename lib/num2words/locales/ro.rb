d6e665823fe2fc8fe75dc4c9678acf0df3f1544f
Ruslan Fedotov <progruson@gmail.com>
2025-08-19 20:29:45 +0700

    Add Romanian lang

---
+++ lib/num2words/locales/ro.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Num2words
+  module Locales
+    module RO
+      ONES_MASC = I18n.t("num2words.ones_masc", locale: :ro)
+      ONES_FEM = I18n.t("num2words.ones_fem", locale: :ro)
+      TEENS = I18n.t("num2words.teens", locale: :ro)
+      TENS = I18n.t("num2words.tens", locale: :ro)
+      HUNDREDS = I18n.t("num2words.hundreds", locale: :ro)
+      SCALES = I18n.t("num2words.scales", locale: :ro)
+
+      RUB = I18n.t("num2words.currencies.RON.name", locale: :ro)
+      KOP = I18n.t("num2words.currencies.RON.fractional.name", locale: :ro)
+    end
+
+    register :ro, RO
+  end
+end
