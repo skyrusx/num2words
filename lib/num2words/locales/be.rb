03a1ba0735deb0e7e9e38570fb6ec325e45657f9
Ruslan Fedotov <progruson@gmail.com>
2025-08-19 20:35:45 +0700

    Add Belarus lang

---
+++ lib/num2words/locales/be.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Num2words
+  module Locales
+    module BE
+      ONES_MASC = I18n.t("num2words.ones_masc", locale: :be)
+      ONES_FEM = I18n.t("num2words.ones_fem", locale: :be)
+      TEENS = I18n.t("num2words.teens", locale: :be)
+      TENS = I18n.t("num2words.tens", locale: :be)
+      HUNDREDS = I18n.t("num2words.hundreds", locale: :be)
+      SCALES = I18n.t("num2words.scales", locale: :be)
+
+      RUB = I18n.t("num2words.currencies.BYN.name", locale: :be)
+      KOP = I18n.t("num2words.currencies.BYN.fractional.name", locale: :be)
+    end
+
+    register :be, BE
+  end
+end
