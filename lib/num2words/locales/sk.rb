53f2cfb484283ec7f3cbe3f6d4ecd0bdd61f428d
Ruslan Fedotov <progruson@gmail.com>
2025-08-19 21:24:15 +0700

    Add Slovak lang

---
+++ lib/num2words/locales/sk.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Num2words
+  module Locales
+    module SK
+      ONES_MASC = I18n.t("num2words.ones_masc", locale: :sk)
+      ONES_FEM = I18n.t("num2words.ones_fem", locale: :sk)
+      TEENS = I18n.t("num2words.teens", locale: :sk)
+      TENS = I18n.t("num2words.tens", locale: :sk)
+      HUNDREDS = I18n.t("num2words.hundreds", locale: :sk)
+      SCALES = I18n.t("num2words.scales", locale: :sk)
+
+      RUB = I18n.t("num2words.currencies.EUR.name", locale: :sk)
+      KOP = I18n.t("num2words.currencies.EUR.fractional.name", locale: :sk)
+    end
+
+    register :sk, SK
+  end
+end
