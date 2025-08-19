fba601a1f074b82594de1c9201ba089aedfd5d5c
Ruslan Fedotov <progruson@gmail.com>
2025-08-19 20:44:14 +0700

    Add Hebrew lang

---
+++ lib/num2words/locales/he.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Num2words
+  module Locales
+    module HE
+      ONES_MASC = I18n.t("num2words.ones_masc", locale: :he)
+      ONES_FEM = I18n.t("num2words.ones_fem", locale: :he)
+      TEENS = I18n.t("num2words.teens", locale: :he)
+      TENS = I18n.t("num2words.tens", locale: :he)
+      HUNDREDS = I18n.t("num2words.hundreds", locale: :he)
+      SCALES = I18n.t("num2words.scales", locale: :he)
+
+      RUB = I18n.t("num2words.currencies.ILS.name", locale: :he)
+      KOP = I18n.t("num2words.currencies.ILS.fractional.name", locale: :he)
+    end
+
+    register :he, HE
+  end
+end
