73403d9eefa0cd9c6ec64697de4a1ad16bcb789e
Ruslan Fedotov <progruson@gmail.com>
2025-08-19 21:28:13 +0700

    Add Lithuanian lang

---
+++ lib/num2words/locales/lt.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Num2words
+  module Locales
+    module LT
+      ONES_MASC = I18n.t("num2words.ones_masc", locale: :lt)
+      ONES_FEM = I18n.t("num2words.ones_fem", locale: :lt)
+      TEENS = I18n.t("num2words.teens", locale: :lt)
+      TENS = I18n.t("num2words.tens", locale: :lt)
+      HUNDREDS = I18n.t("num2words.hundreds", locale: :lt)
+      SCALES = I18n.t("num2words.scales", locale: :lt)
+
+      RUB = I18n.t("num2words.currencies.EUR.name", locale: :lt)
+      KOP = I18n.t("num2words.currencies.EUR.fractional.name", locale: :lt)
+    end
+
+    register :lt, LT
+  end
+end
