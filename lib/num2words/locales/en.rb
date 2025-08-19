d64505adb9eca092744d979aa5444844fc3f6212
Ruslan Fedotov <progruson@gmail.com>
2025-08-19 16:58:57 +0700

    Add Num2words::Locales::EN

---
+++ lib/num2words/locales/en.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Num2words
+  module Locales
+    module EN
+      ONES_MASC = I18n.t("num2words.ones_masc", locale: :en)
+      ONES_FEM = I18n.t("num2words.ones_fem", locale: :en)
+      TEENS = I18n.t("num2words.teens", locale: :en)
+      TENS = I18n.t("num2words.tens", locale: :en)
+      HUNDREDS = I18n.t("num2words.hundreds", locale: :en)
+      SCALES = I18n.t("num2words.scales", locale: :en)
+
+      RUB = I18n.t("num2words.currencies.RUB.name", locale: :en)
+      KOP = I18n.t("num2words.currencies.RUB.fractional.name", locale: :en)
+    end
+
+    register :en, EN
+  end
+end
