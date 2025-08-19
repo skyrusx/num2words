9dcd02730e70cf2339e6a54e06d796c7d56ac860
Ruslan Fedotov <progruson@gmail.com>
2025-08-19 21:39:31 +0700

    Add Urdu lang

---
+++ lib/num2words/locales/ur.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Num2words
+  module Locales
+    module UR
+      ONES_MASC = I18n.t("num2words.ones_masc", locale: :ur)
+      ONES_FEM = I18n.t("num2words.ones_fem", locale: :ur)
+      TEENS = I18n.t("num2words.teens", locale: :ur)
+      TENS = I18n.t("num2words.tens", locale: :ur)
+      HUNDREDS = I18n.t("num2words.hundreds", locale: :ur)
+      SCALES = I18n.t("num2words.scales", locale: :ur)
+
+      RUB = I18n.t("num2words.currencies.PKR.name", locale: :ur)
+      KOP = I18n.t("num2words.currencies.PKR.fractional.name", locale: :ur)
+    end
+
+    register :ur, UR
+  end
+end
