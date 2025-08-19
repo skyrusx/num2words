0566cb958c89b4278fd47e948261a224a6ad2382
Ruslan Fedotov <progruson@gmail.com>
2025-08-19 21:43:46 +0700

    Add Telugu lang

---
+++ lib/num2words/locales/te.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Num2words
+  module Locales
+    module TE
+      ONES_MASC = I18n.t("num2words.ones_masc", locale: :te)
+      ONES_FEM = I18n.t("num2words.ones_fem", locale: :te)
+      TEENS = I18n.t("num2words.teens", locale: :te)
+      TENS = I18n.t("num2words.tens", locale: :te)
+      HUNDREDS = I18n.t("num2words.hundreds", locale: :te)
+      SCALES = I18n.t("num2words.scales", locale: :te)
+
+      RUB = I18n.t("num2words.currencies.INR.name", locale: :te)
+      KOP = I18n.t("num2words.currencies.INR.fractional.name", locale: :te)
+    end
+
+    register :te, TE
+  end
+end
