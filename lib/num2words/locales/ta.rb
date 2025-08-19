c475856397e5034912cacf13860d4646f51297d5
Ruslan Fedotov <progruson@gmail.com>
2025-08-19 21:41:36 +0700

    Add Tamil lang

---
+++ lib/num2words/locales/ta.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Num2words
+  module Locales
+    module TA
+      ONES_MASC = I18n.t("num2words.ones_masc", locale: :ta)
+      ONES_FEM = I18n.t("num2words.ones_fem", locale: :ta)
+      TEENS = I18n.t("num2words.teens", locale: :ta)
+      TENS = I18n.t("num2words.tens", locale: :ta)
+      HUNDREDS = I18n.t("num2words.hundreds", locale: :ta)
+      SCALES = I18n.t("num2words.scales", locale: :ta)
+
+      RUB = I18n.t("num2words.currencies.INR.name", locale: :ta)
+      KOP = I18n.t("num2words.currencies.INR.fractional.name", locale: :ta)
+    end
+
+    register :ta, TA
+  end
+end
