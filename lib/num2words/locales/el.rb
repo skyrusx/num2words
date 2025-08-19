b18510d4a214cb0e1e0c4b3f090c893fe7a8d97a
Ruslan Fedotov <progruson@gmail.com>
2025-08-19 20:42:05 +0700

    Add Greek lang

---
+++ lib/num2words/locales/el.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Num2words
+  module Locales
+    module EL
+      ONES_MASC = I18n.t("num2words.ones_masc", locale: :el)
+      ONES_FEM = I18n.t("num2words.ones_fem", locale: :el)
+      TEENS = I18n.t("num2words.teens", locale: :el)
+      TENS = I18n.t("num2words.tens", locale: :el)
+      HUNDREDS = I18n.t("num2words.hundreds", locale: :el)
+      SCALES = I18n.t("num2words.scales", locale: :el)
+
+      RUB = I18n.t("num2words.currencies.EUR.name", locale: :el)
+      KOP = I18n.t("num2words.currencies.EUR.fractional.name", locale: :el)
+    end
+
+    register :el, EL
+  end
+end
