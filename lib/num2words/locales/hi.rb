2e544cbce8581e81f9e02fa89eaf8cd7b8136dd7
Ruslan Fedotov <progruson@gmail.com>
2025-08-19 18:40:13 +0700

    Add Hindi lang

---
+++ lib/num2words/locales/hi.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Num2words
+  module Locales
+    module HI
+      ONES_MASC = I18n.t("num2words.ones_masc", locale: :hi)
+      ONES_FEM = I18n.t("num2words.ones_fem", locale: :hi)
+      TEENS = I18n.t("num2words.teens", locale: :hi)
+      TENS = I18n.t("num2words.tens", locale: :hi)
+      HUNDREDS = I18n.t("num2words.hundreds", locale: :hi)
+      SCALES = I18n.t("num2words.scales", locale: :hi)
+
+      RUB = I18n.t("num2words.currencies.INR.name", locale: :hi)
+      KOP = I18n.t("num2words.currencies.INR.fractional.name", locale: :hi)
+    end
+
+    register :hi, HI
+  end
+end
