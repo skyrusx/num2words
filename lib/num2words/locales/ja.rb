a36b904f7a15da966705ad9f0b36fc1c40a80ca7
Ruslan Fedotov <progruson@gmail.com>
2025-08-19 18:42:06 +0700

    Add Japanese lang

---
+++ lib/num2words/locales/ja.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Num2words
+  module Locales
+    module JA
+      ONES_MASC = I18n.t("num2words.ones_masc", locale: :ja)
+      ONES_FEM = I18n.t("num2words.ones_fem", locale: :ja)
+      TEENS = I18n.t("num2words.teens", locale: :ja)
+      TENS = I18n.t("num2words.tens", locale: :ja)
+      HUNDREDS = I18n.t("num2words.hundreds", locale: :ja)
+      SCALES = I18n.t("num2words.scales", locale: :ja)
+
+      RUB = I18n.t("num2words.currencies.JPY.name", locale: :ja)
+      KOP = I18n.t("num2words.currencies.JPY.fractional.name", locale: :ja)
+    end
+
+    register :ja, JA
+  end
+end
