d1c9c61d7026ffd8da42be18bb49abf0810e0d62
Ruslan Fedotov <progruson@gmail.com>
2025-08-19 18:58:36 +0700

    Add Dutch lang

---
+++ lib/num2words/locales/nl.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Num2words
+  module Locales
+    module NL
+      ONES_MASC = I18n.t("num2words.ones_masc", locale: :nl)
+      ONES_FEM = I18n.t("num2words.ones_fem", locale: :nl)
+      TEENS = I18n.t("num2words.teens", locale: :nl)
+      TENS = I18n.t("num2words.tens", locale: :nl)
+      HUNDREDS = I18n.t("num2words.hundreds", locale: :nl)
+      SCALES = I18n.t("num2words.scales", locale: :nl)
+
+      RUB = I18n.t("num2words.currencies.EUR.name", locale: :nl)
+      KOP = I18n.t("num2words.currencies.EUR.fractional.name", locale: :nl)
+    end
+
+    register :nl, NL
+  end
+end
