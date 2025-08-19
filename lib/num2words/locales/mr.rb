ce980715dede50883f6c855ec4c7d7f2c6978ee0
Ruslan Fedotov <progruson@gmail.com>
2025-08-19 21:49:12 +0700

    Add Marathi lang

---
+++ lib/num2words/locales/mr.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Num2words
+  module Locales
+    module MR
+      ONES_MASC = I18n.t("num2words.ones_masc", locale: :mr)
+      ONES_FEM = I18n.t("num2words.ones_fem", locale: :mr)
+      TEENS = I18n.t("num2words.teens", locale: :mr)
+      TENS = I18n.t("num2words.tens", locale: :mr)
+      HUNDREDS = I18n.t("num2words.hundreds", locale: :mr)
+      SCALES = I18n.t("num2words.scales", locale: :mr)
+
+      RUB = I18n.t("num2words.currencies.INR.name", locale: :mr)
+      KOP = I18n.t("num2words.currencies.INR.fractional.name", locale: :mr)
+    end
+
+    register :mr, MR
+  end
+end
