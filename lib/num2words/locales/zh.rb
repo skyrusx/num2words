c5f2dbae440598c5f8086c03f31157093e0798ec
Ruslan Fedotov <progruson@gmail.com>
2025-08-19 17:59:19 +0700

    Add Chinese lang

---
+++ lib/num2words/locales/zh.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Num2words
+  module Locales
+    module ZH
+      ONES_MASC = I18n.t("num2words.ones_masc", locale: :zh)
+      ONES_FEM = I18n.t("num2words.ones_fem", locale: :zh)
+      TEENS = I18n.t("num2words.teens", locale: :zh)
+      TENS = I18n.t("num2words.tens", locale: :zh)
+      HUNDREDS = I18n.t("num2words.hundreds", locale: :zh)
+      SCALES = I18n.t("num2words.scales", locale: :zh)
+
+      RUB = I18n.t("num2words.currencies.CNY.name", locale: :zh)
+      KOP = I18n.t("num2words.currencies.CNY.fractional.name", locale: :zh)
+    end
+
+    register :zh, ZH
+  end
+end
