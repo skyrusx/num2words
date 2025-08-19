98bde3e4a6ec1578243ade402e7e0bb599be916c
Ruslan Fedotov <progruson@gmail.com>
2025-08-19 21:11:41 +0700

    Add Indonesian lang

---
+++ lib/num2words/locales/id.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Num2words
+  module Locales
+    module ID
+      ONES_MASC = I18n.t("num2words.ones_masc", locale: :id)
+      ONES_FEM = I18n.t("num2words.ones_fem", locale: :id)
+      TEENS = I18n.t("num2words.teens", locale: :id)
+      TENS = I18n.t("num2words.tens", locale: :id)
+      HUNDREDS = I18n.t("num2words.hundreds", locale: :id)
+      SCALES = I18n.t("num2words.scales", locale: :id)
+
+      RUB = I18n.t("num2words.currencies.IDR.name", locale: :id)
+      KOP = I18n.t("num2words.currencies.IDR.fractional.name", locale: :id)
+    end
+
+    register :id, ID
+  end
+end
