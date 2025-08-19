cfa0f80029ddb84c3e3b50ddd902c121dd1b0ea7
Ruslan Fedotov <progruson@gmail.com>
2025-08-19 21:50:59 +0700

    Add Gujarati lang

---
+++ lib/num2words/locales/gu.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Num2words
+  module Locales
+    module GU
+      ONES_MASC = I18n.t("num2words.ones_masc", locale: :gu)
+      ONES_FEM = I18n.t("num2words.ones_fem", locale: :gu)
+      TEENS = I18n.t("num2words.teens", locale: :gu)
+      TENS = I18n.t("num2words.tens", locale: :gu)
+      HUNDREDS = I18n.t("num2words.hundreds", locale: :gu)
+      SCALES = I18n.t("num2words.scales", locale: :gu)
+
+      RUB = I18n.t("num2words.currencies.INR.name", locale: :gu)
+      KOP = I18n.t("num2words.currencies.INR.fractional.name", locale: :gu)
+    end
+
+    register :gu, GU
+  end
+end
