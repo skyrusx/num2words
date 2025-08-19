8110a314c5f0320028dac566c27e8dd7dba777c8
Ruslan Fedotov <progruson@gmail.com>
2025-08-19 20:38:39 +0700

    Add Kazakhstan lang

---
+++ lib/num2words/locales/kz.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Num2words
+  module Locales
+    module KZ
+      ONES_MASC = I18n.t("num2words.ones_masc", locale: :kz)
+      ONES_FEM = I18n.t("num2words.ones_fem", locale: :kz)
+      TEENS = I18n.t("num2words.teens", locale: :kz)
+      TENS = I18n.t("num2words.tens", locale: :kz)
+      HUNDREDS = I18n.t("num2words.hundreds", locale: :kz)
+      SCALES = I18n.t("num2words.scales", locale: :kz)
+
+      RUB = I18n.t("num2words.currencies.KZT.name", locale: :kz)
+      KOP = I18n.t("num2words.currencies.KZT.fractional.name", locale: :kz)
+    end
+
+    register :kz, KZ
+  end
+end
