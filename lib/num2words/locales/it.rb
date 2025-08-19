9e5e2a315aafa09ececf2c1c89e5327495f894ca
Ruslan Fedotov <progruson@gmail.com>
2025-08-19 18:21:51 +0700

    Add Italian lang

---
+++ lib/num2words/locales/it.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Num2words
+  module Locales
+    module IT
+      ONES_MASC = I18n.t("num2words.ones_masc", locale: :it)
+      ONES_FEM = I18n.t("num2words.ones_fem", locale: :it)
+      TEENS = I18n.t("num2words.teens", locale: :it)
+      TENS = I18n.t("num2words.tens", locale: :it)
+      HUNDREDS = I18n.t("num2words.hundreds", locale: :it)
+      SCALES = I18n.t("num2words.scales", locale: :it)
+
+      RUB = I18n.t("num2words.currencies.EUR.name", locale: :it)
+      KOP = I18n.t("num2words.currencies.EUR.fractional.name", locale: :it)
+    end
+
+    register :it, IT
+  end
+end
