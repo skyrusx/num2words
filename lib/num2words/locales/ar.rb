588e4e6eedd852090de6f074974bb3888bc7f3d8
Ruslan Fedotov <progruson@gmail.com>
2025-08-19 18:29:26 +0700

    Add Arabic lang

---
+++ lib/num2words/locales/ar.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Num2words
+  module Locales
+    module AR
+      ONES_MASC = I18n.t("num2words.ones_masc", locale: :ar)
+      ONES_FEM = I18n.t("num2words.ones_fem", locale: :ar)
+      TEENS = I18n.t("num2words.teens", locale: :ar)
+      TENS = I18n.t("num2words.tens", locale: :ar)
+      HUNDREDS = I18n.t("num2words.hundreds", locale: :ar)
+      SCALES = I18n.t("num2words.scales", locale: :ar)
+
+      RUB = I18n.t("num2words.currencies.SAR.name", locale: :ar)
+      KOP = I18n.t("num2words.currencies.SAR.fractional.name", locale: :ar)
+    end
+
+    register :ar, AR
+  end
+end
