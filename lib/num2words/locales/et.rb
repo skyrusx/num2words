f24b7320444e5d7495f9ddf1221082e17e13ed42
Ruslan Fedotov <progruson@gmail.com>
2025-08-19 21:33:04 +0700

    Add Estonian lang

---
+++ lib/num2words/locales/et.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Num2words
+  module Locales
+    module ET
+      ONES_MASC = I18n.t("num2words.ones_masc", locale: :et)
+      ONES_FEM = I18n.t("num2words.ones_fem", locale: :et)
+      TEENS = I18n.t("num2words.teens", locale: :et)
+      TENS = I18n.t("num2words.tens", locale: :et)
+      HUNDREDS = I18n.t("num2words.hundreds", locale: :et)
+      SCALES = I18n.t("num2words.scales", locale: :et)
+
+      RUB = I18n.t("num2words.currencies.EUR.name", locale: :et)
+      KOP = I18n.t("num2words.currencies.EUR.fractional.name", locale: :et)
+    end
+
+    register :et, ET
+  end
+end
