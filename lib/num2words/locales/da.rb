3416ffa2d3d54db54281e3dd7f822a64bba099b2
Ruslan Fedotov <progruson@gmail.com>
2025-08-19 20:59:02 +0700

    Add Danish lang

---
+++ lib/num2words/locales/da.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Num2words
+  module Locales
+    module DA
+      ONES_MASC = I18n.t("num2words.ones_masc", locale: :da)
+      ONES_FEM = I18n.t("num2words.ones_fem", locale: :da)
+      TEENS = I18n.t("num2words.teens", locale: :da)
+      TENS = I18n.t("num2words.tens", locale: :da)
+      HUNDREDS = I18n.t("num2words.hundreds", locale: :da)
+      SCALES = I18n.t("num2words.scales", locale: :da)
+
+      RUB = I18n.t("num2words.currencies.DKK.name", locale: :da)
+      KOP = I18n.t("num2words.currencies.DKK.fractional.name", locale: :da)
+    end
+
+    register :da, DA
+  end
+end
