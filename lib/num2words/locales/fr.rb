16cba9d597703dde9d6f593b84e3c37db3bf2a04
Ruslan Fedotov <progruson@gmail.com>
2025-08-19 18:15:56 +0700

    Add French lang

---
+++ lib/num2words/locales/fr.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Num2words
+  module Locales
+    module FR
+      ONES_MASC = I18n.t("num2words.ones_masc", locale: :fr)
+      ONES_FEM = I18n.t("num2words.ones_fem", locale: :fr)
+      TEENS = I18n.t("num2words.teens", locale: :fr)
+      TENS = I18n.t("num2words.tens", locale: :fr)
+      HUNDREDS = I18n.t("num2words.hundreds", locale: :fr)
+      SCALES = I18n.t("num2words.scales", locale: :fr)
+
+      RUB = I18n.t("num2words.currencies.EUR.name", locale: :fr)
+      KOP = I18n.t("num2words.currencies.EUR.fractional.name", locale: :fr)
+    end
+
+    register :fr, FR
+  end
+end
