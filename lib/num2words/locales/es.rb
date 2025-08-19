6e9403279ff62ee43d4a9e5c89e037f0fca7ab13
Ruslan Fedotov <progruson@gmail.com>
2025-08-19 18:13:39 +0700

    Add Spanish lang

---
+++ lib/num2words/locales/es.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Num2words
+  module Locales
+    module ES
+      ONES_MASC = I18n.t("num2words.ones_masc", locale: :es)
+      ONES_FEM = I18n.t("num2words.ones_fem", locale: :es)
+      TEENS = I18n.t("num2words.teens", locale: :es)
+      TENS = I18n.t("num2words.tens", locale: :es)
+      HUNDREDS = I18n.t("num2words.hundreds", locale: :es)
+      SCALES = I18n.t("num2words.scales", locale: :es)
+
+      RUB = I18n.t("num2words.currencies.EUR.name", locale: :es)
+      KOP = I18n.t("num2words.currencies.EUR.fractional.name", locale: :es)
+    end
+
+    register :es, ES
+  end
+end
