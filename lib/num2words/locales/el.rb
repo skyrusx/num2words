8dca9b18bfa99fb253e07d1798624ac26495f7aa
Ruslan Fedotov <progruson@gmail.com>
2025-08-23 18:37:39 +0700

    Fix el locale

--- lib/num2words/locales/el.rb
+++ lib/num2words/locales/el.rb
@@ -10,8 +10,16 @@ module Num2words
       HUNDREDS = I18n.t("num2words.hundreds", locale: :el)
       SCALES = I18n.t("num2words.scales", locale: :el)
 
-      MAJOR_UNIT = I18n.t("num2words.currencies.EUR.name", locale: :el)
-      MINOR_UNIT = I18n.t("num2words.currencies.EUR.fractional.name", locale: :el)
+      FRACTIONS = I18n.t("num2words.fractions", locale: :el)
+      GRAMMAR = I18n.t("num2words.grammar", locale: :el)
+
+      DATE = I18n.t("num2words.date", locale: :el)
+      DATE_TEMPLATE = I18n.t("num2words.date.template", locale: :el)
+      TIME = I18n.t("num2words.time", locale: :el)
+      TIME_TEMPLATE = I18n.t("num2words.time.template", locale: :el)
+      DATETIME_TEMPLATE = I18n.t("num2words.datetime.template", locale: :el)
+
+      ORDINALS = I18n.t("num2words.numbers.ordinals", locale: :el)
     end
 
     register :el, EL
